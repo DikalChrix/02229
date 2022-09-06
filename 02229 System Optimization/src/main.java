9d5e0253933e1a23b9f5aec5b26758af2324611e
DikalChrix <43571440+DikalChrix@users.noreply.github.com>
2022-09-06 16:09:37 +0200

    Setup

---
+++ 02229 System Optimization/src/main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+import java.time.*;
+
+public class main {
+	public static void main(String[] args) {
+		
+	}
+}
